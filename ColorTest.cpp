adcaffe028d68bc3133e310daee68baae4d49011
elescant <2475199146@qq.com>
2018-11-28 05:47:01 -0800

    init

---
+++ ColorTest.cpp
@@ -0,0 +1,25 @@
+#include "ColorTest.h"
+#include "ui_ColorTest.h"
+
+ColorTest::ColorTest(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ColorTest),
+    m_pColorDlg(NULL)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(showColorDlg()));
+}
+
+ColorTest::~ColorTest()
+{
+    delete ui;
+}
+
+void ColorTest::showColorDlg()
+{
+    if(m_pColorDlg == NULL)
+    {
+        m_pColorDlg = new ColorDialog(this);
+    }
+    m_pColorDlg->showDialog();
+}
---
+++ PreviewColorArea.h
@@ -0,0 +1,39 @@
+/************************************************************************/
+/** 颜色预览区域
+/************************************************************************/
+
+#ifndef PREVIEWCOLORAREA_H
+#define PREVIEWCOLORAREA_H
+
+#include <QWidget>
+
+class PreviewColorArea : public QWidget
+{
+	Q_OBJECT
+
+public:
+	PreviewColorArea(QWidget *parent);
+	~PreviewColorArea();
+
+	void setCurColor(const QColor &);
+	void setNewColor(const QColor &);
+
+protected:
+	void paintEvent(QPaintEvent *);
+	void resizeEvent(QResizeEvent *);
+
+private:
+	void paint(QPainter &painter, QRect) const;
+
+public slots:
+	void svChangedSlot(int, int, int);
+	
+signals:
+	void svChangedSignal(int, int, int);
+
+private:
+	QColor m_curColor;
+	QColor m_newColor;
+};
+
+#endif // PREVIEWCOLORAREA_H
